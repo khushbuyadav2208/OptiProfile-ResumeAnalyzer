3a1680bf019a5455d20db59c338d896cfa32908a
Khushbu Yadav <khushbuyadav2208@gmail.com>
2025-11-30 12:52:04 +0530

    Added google OAuth + updated server.js

--- backend/middlewares/passportGoogle.js
+++ backend/middlewares/passportGoogle.js
@@ -1,50 +1,47 @@
 import passport from "passport";
-import GoogleStrategy from "passport-google-oauth20";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import userModel from "../models/userModel.js";
-import JWT from "jsonwebtoken";
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:
-        "https://optiprofile-resumeanalyzer.onrender.com/api/v1/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
-        let email = profile.emails[0].value;
+        const email = profile.emails[0].value;
 
-        let user = await userModel.findOne({ email });
+        let user = await userModel.findOne({ googleId: profile.id });
 
         if (!user) {
-          user = await userModel.create({
-            name: profile.displayName,
-            email: email,
-            password: "google_oauth_user",
-          });
+          user = await userModel.findOne({ email });
+
+          if (user) {
+            user.googleId = profile.id;
+            await user.save();
+          } else {
+            user = await userModel.create({
+              name: profile.displayName,
+              email: email,
+              googleId: profile.id,
+            });
+          }
         }
 
-        const token = JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
-          expiresIn: "7d",
-        });
-
-        user.token = token;
-
         return done(null, user);
-      } catch (error) {
-        console.log("Google Auth Error:", error);
-        return done(error, null);
+      } catch (err) {
+        return done(err, null);
       }
     }
   )
 );
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
+passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
   const user = await userModel.findById(id);
   done(null, user);
 });
+
+export default passport;
